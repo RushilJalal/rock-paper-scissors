629a23c9a35e00ee61428a0f5b2d99b244c8a122
Rushil Jalal <rushiljalal@gmail.com>
2023-06-29 23:24:48 +0530

    Add game functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 choiceList = ['rock', 'paper', 'scissors'];
+let playerScore = compScore = 0;
 
 //choice played by computer
 //returns a randomly generated choice from the available choices in choiceList. 
@@ -8,20 +9,42 @@ function getComputerChoice() {
     return compChoice;
 }
 
+//Consists of game logic and returns round result 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
 
-    if (playerSelection == 'rock' && computerSelection === 'scissors' || playerSelection == 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper')
-        return "You won! Computer lost!"
+    if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerScore++;
+        return `You won! Computer lost! \n Player:${playerScore}, Computer:${compScore}`
+    }
 
-    else if (playerSelection == 'scissors' && computerSelection === 'rock' || playerSelection == 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors')
-        return "You lose! Computer won!"
+    else if (playerSelection === 'scissors' && computerSelection === 'rock' || playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors') {
+        compScore++;
+        return `You lose! Computer won! \n Player:${playerScore}, Computer:${compScore}`
+    }
 
-    else if (playerSelection === computerSelection)
-        return "It's a tie!"
+    else if (playerSelection === computerSelection) {
+        return `It's a tie! \n Player:${playerScore}, Computer:${compScore}`
+    }
 
     else
         return "Invalid"
 }
+
+//loops over playRound until either user or computer reach 5 points
+function game() {
+    while (playerScore < 5 && compScore < 5) {
+        playerSelection = prompt("Enter your choice");
+        computerSelection = getComputerChoice();
+        alert(playRound(playerSelection, computerSelection));
+    }
+
+    if (playerScore === 5)
+        alert("Player won the game!");
+
+    else
+        alert("Computer won the game!");
+}
+
+game();
 
